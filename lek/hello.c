b19c91a70f3bd700f66e017cae76f56dac8a11a0
Arash Rouhani <rarash@gatech.edu>
2013-03-08 16:51:12 -0500

    Work

---
+++ lek/hello.c
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/08/2013 02:01:56 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+  int arr[2];
+  int b = 123;
+  arr[b];
+  /* a = arr[17+b]; */
+  /* int a; */
+  /* int b = 5; */
+  /* printf("%d\n", b); */
+}
+
+/* unsigned int sum(unsigned int a){ */
+/*   unsigned int acc = 0; */
+/*   for(unsigned int i = 0; i < a; i++){ */
+/*     acc+=i; */
+/*   } */
+/*   return acc; */
+/* } */
